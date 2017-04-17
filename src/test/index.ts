7f9e59ba301b31baa37064a7f144ed4798732330
whitecolor <alex.oshchepkov@gmail.com>
2017-04-17 13:22:54 +0500

    tests skeleton

---
+++ src/test/index.ts
@@ -0,0 +1,33 @@
+
+
+describe('YLoc package manager', () => {
+  describe('Package publish', () => {
+
+    it('handles .npmignore correctly', () => {
+      
+    })
+    
+    it('handles `files` manifest entry correctly', () => {
+      
+    })
+
+    it('.gitignore', () => {
+      
+    })
+  })
+
+  describe('Package add', () => {
+
+    it('updates installations file', () => {
+
+    })
+  })
+
+  describe('Package update', () => {
+
+  })
+
+  describe('Package remove', () => {
+
+  })
+})
