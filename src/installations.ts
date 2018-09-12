936711d9637e4a6879deb524879b506610581dd8
greyepoxy <greyepoxy2@gmail.com>
2018-09-12 13:47:17 -0700

    style: add auto formatter and format everything
    Use vscode tslint plugin to autoFormat on save or run `yarn run lint --fix` in the command line

--- src/installations.ts
+++ src/installations.ts
@@ -5,10 +5,10 @@ import { getStoreMainDir, values } from '.'
 export type PackageName = string & { __packageName: true }
 
 export type PackageInstallation = {
-  name: PackageName,
-  version: string,
-  path: string,
-  replaced?: string, 
+  name: PackageName
+  version: string
+  path: string
+  replaced?: string
   signature?: string
 }
 
@@ -16,18 +16,18 @@ export type InstallationsFile = { [packageName: string]: string[] }
 
 export const readInstallationsFile = (): InstallationsFile => {
   const storeDir = getStoreMainDir()
-  const installationFilePath = path.join(storeDir, values.installationsFile)  
+  const installationFilePath = path.join(storeDir, values.installationsFile)
   let installationsConfig: InstallationsFile
-  
-  try {    
-    fs.accessSync(installationFilePath)  
+
+  try {
+    fs.accessSync(installationFilePath)
     try {
       installationsConfig = fs.readJsonSync(installationFilePath)
     } catch (e) {
       console.log('Error reading installations file', installationFilePath, e)
       installationsConfig = {}
     }
-  } catch (e) {    
+  } catch (e) {
     installationsConfig = {}
   }
 
@@ -44,17 +44,15 @@ export const saveInstallationsFile = async (installationsConfig: InstallationsFi
 export const addInstallations = async (installations: (PackageInstallation)[]) => {
   const installationsConfig = readInstallationsFile()
   let updated = false
-  installations
-    .forEach(newInstall => {
-      const packageInstallPaths = installationsConfig[newInstall.name] || []
-      installationsConfig[newInstall.name] = packageInstallPaths
-      const hasInstallation = !!packageInstallPaths
-        .filter(p => p === newInstall.path)[0]
-      if (!hasInstallation) {
-        updated = true
-        packageInstallPaths.push(newInstall!.path)
-      }
-    })
+  installations.forEach(newInstall => {
+    const packageInstallPaths = installationsConfig[newInstall.name] || []
+    installationsConfig[newInstall.name] = packageInstallPaths
+    const hasInstallation = !!packageInstallPaths.filter(p => p === newInstall.path)[0]
+    if (!hasInstallation) {
+      updated = true
+      packageInstallPaths.push(newInstall!.path)
+    }
+  })
 
   if (updated) {
     await saveInstallationsFile(installationsConfig)
@@ -64,18 +62,17 @@ export const addInstallations = async (installations: (PackageInstallation)[]) =
 export const removeInstallations = async (installations: (PackageInstallation)[]) => {
   const installationsConfig = readInstallationsFile()
   let updated = false
-  installations
-    .forEach(install => {
-      const packageInstallPaths = installationsConfig[install.name] || []
-      const index = packageInstallPaths.indexOf(install.path)
-      if (index >= 0) {
-        packageInstallPaths.splice(index, 1)
-        updated = true
-      }
-      if (!packageInstallPaths.length) {
-        delete installationsConfig[install.name]
-      }
-    })
+  installations.forEach(install => {
+    const packageInstallPaths = installationsConfig[install.name] || []
+    const index = packageInstallPaths.indexOf(install.path)
+    if (index >= 0) {
+      packageInstallPaths.splice(index, 1)
+      updated = true
+    }
+    if (!packageInstallPaths.length) {
+      delete installationsConfig[install.name]
+    }
+  })
   if (updated) {
     await saveInstallationsFile(installationsConfig)
   }
