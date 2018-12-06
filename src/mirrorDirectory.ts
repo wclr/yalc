906f53b514351b3d399af2ab22473a787615d6e2
greyepoxy <greyepoxy2@gmail.com>
2018-12-06 11:45:08 -0800

    fix: add ensureItemRemoved call to make sure that file has actually been removed before copy call

--- src/mirrorDirectory.ts
+++ src/mirrorDirectory.ts
@@ -35,7 +35,11 @@ export async function mirrorDirectory(
     itemsWithChangedContents
   )
 
-  await removeItems(destinationDirectory, itemsWithChangedTypes)
+  await removeItems(
+    destinationDirectory,
+    itemsWithChangedTypes,
+    ensureItemRemoved
+  )
   await copyItems(sourceDirectory, destinationDirectory, itemsWithChangedTypes)
 
   await removeItems(destinationDirectory, itemsToRemove)
@@ -286,23 +290,106 @@ async function copyItems(
   dirToCopyTo: string,
   itemRelativePaths: string[]
 ): Promise<void> {
-  const itemsToCopy = itemRelativePaths.map(itemRelativePath => {
+  const itemsToCopy = itemRelativePaths.map(async itemRelativePath => {
     const sourceItemPath = path.resolve(dirToCopyFrom, itemRelativePath)
     const destinationItemPath = path.resolve(dirToCopyTo, itemRelativePath)
 
-    return fs.copy(sourceItemPath, destinationItemPath, {
-      preserveTimestamps: true
-    })
+    try {
+      await fs.copy(sourceItemPath, destinationItemPath, {
+        preserveTimestamps: true
+      })
+    } catch (error) {
+      console.error(
+        `Unable to copy item ${sourceItemPath} to ${destinationItemPath}`
+      )
+      console.error(error)
+    }
   })
   await Promise.all(itemsToCopy)
 }
 
 async function removeItems(
   dirToRemoveFrom: string,
-  itemsRelativePaths: string[]
+  itemsRelativePaths: string[],
+  removeItemFunction: (itemAbsolutePath: string) => Promise<void> = removeItem
 ): Promise<void> {
-  const removeItemsPromises = itemsRelativePaths.map(itemRelativePath =>
-    fs.remove(path.resolve(dirToRemoveFrom, itemRelativePath))
-  )
+  const removeItemsPromises = itemsRelativePaths.map(async itemRelativePath => {
+    const pathToItem = path.resolve(dirToRemoveFrom, itemRelativePath)
+    await removeItemFunction(pathToItem)
+  })
   await Promise.all(removeItemsPromises)
+}
+
+async function removeItem(itemAbsolutePath: string): Promise<void> {
+  try {
+    await fs.remove(itemAbsolutePath)
+  } catch (error) {
+    reportUnableToRemoveItem(itemAbsolutePath, error)
+  }
+}
+
+async function ensureItemRemoved(itemAbsolutePath: string): Promise<void> {
+  try {
+    await fs.remove(itemAbsolutePath)
+    if (await doesItemStillExist(itemAbsolutePath)) {
+      // try once again after 100 ms, this should be rarely needed
+      await delay(100)
+      await doesItemStillExist(itemAbsolutePath, true /*rejectIfExists*/)
+    }
+  } catch (error) {
+    reportUnableToRemoveItem(itemAbsolutePath, error)
+  }
+}
+
+function reportUnableToRemoveItem(itemAbsolutePath: string, error: any): void {
+  console.error(`Unable to remove item ${itemAbsolutePath}`)
+  console.error(error)
+}
+
+function delay<T>(msToDelay: number, valueToPassThrough?: T) {
+  return new Promise<T>(function(resolve) {
+    setTimeout(resolve.bind(null, valueToPassThrough), msToDelay)
+  })
+}
+
+async function doesItemStillExist(
+  itemAbsolutePath: string,
+  rejectIfExists: boolean = false
+): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    fs.access(itemAbsolutePath, err => {
+      if (err === null) {
+        return rejectIfExists
+          ? reject(new Error(`Path exists ${itemAbsolutePath}`))
+          : resolve(true)
+      }
+
+      if (err.code !== undefined && err.code === 'ENOENT') {
+        // No such file or directory, the expected error when the file no longer
+        // exists
+        return resolve(false)
+      }
+
+      if (err.code !== undefined && err.code === 'EPERM') {
+        // Operation not permitted, on windows if another process is also
+        // accessing a file the file won't be deleted immediately and instead
+        // will be put into a 'PENDING DELETE' state. This will result in the
+        // file 'existing' but permission errors occurring when trying to access
+        // it
+        return rejectIfExists
+          ? reject(
+              new Error(
+                `Path exists ${itemAbsolutePath} but received ${
+                  err.code
+                } when accessing it. ` +
+                  `If a remove call has just been made this likely means the item is in a 'PENDING DELETE' state due to another process still accessing it.`
+              )
+            )
+          : resolve(true)
+      }
+
+      // An unexpected error occurred
+      reject(err)
+    })
+  })
 }
